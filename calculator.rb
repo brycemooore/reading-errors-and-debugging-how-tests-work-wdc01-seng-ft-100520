85824cf1346c9eef3233ba0af7359e6be8221e1f
Bryce Moore <brycemooore@gmail.com>
2020-09-04 22:40:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 # Add your variables here
+first_number = 6
+second_number = 47
+sum = first_number + second_number
+quotient = first_number.to_f / second_number.to_f
+product = first_number * second_number
+difference = first_number - second_number
